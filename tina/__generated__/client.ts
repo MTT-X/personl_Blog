934d98ed9705677332d487edf053cb2097bd937f
Developer <dev@blog.local>
2026-05-24 19:37:50 +0800

    build: add tina generated schema and type files for admin panel

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,2 +1,5 @@
-export default ()=>({})
-export const client = ()=>({})
+import { createClient } from "tinacms/dist/client";
+import { queries } from "./types";
+export const client = createClient({ cacheDir: 'E:/个人博客网站项目/tina/__generated__/.cache/1779622442951', url: 'https://content.tinajs.io/1.6/content/test/github/main', token: 'test', queries,  });
+export default client;
+  
